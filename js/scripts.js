4eff7e17746aa2d8cf610bea298ed31752a0402d
Azul Rodriguez Da Silva <azulejx@gmail.com>
2018-10-31 21:02:13 -0700

    add starter script language

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//BUSINESS LOGIC
+var orderTotal = {
+  total: "none"
+};
+
+var flowers = [];
+var size = "";
+
+function Order(flowers, size) = {
+  this.flowers = flowers;
+  this.size = size;
+};
+
+var flowerSelection = flowers.forEach(function(flower) {
+  total += flowers;
+});
+
+Order.prototype.total = function() {
+  flowerSelection * this.size;
+};
+
+//UI LOGIC
